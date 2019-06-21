7d1e12546fe9005a050ef34c91f357e180cf8ef9
Jarel Perullo <drperullo@gmail.com>
2019-06-21 16:32:30 -0400

    initial commit

---
+++ src/test/java/perullo/petprojects/measurements/MeasurementTest.java
@@ -0,0 +1,14 @@
+package perullo.petprojects.measurements;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MeasurementTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
