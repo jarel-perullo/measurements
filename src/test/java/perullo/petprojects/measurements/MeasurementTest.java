f6ea568ccf77dcd0bc0d3275f3c5505e05ec480b
Jarel Perullo <drperullo@gmail.com>
2019-06-22 08:14:36 -0400

    refactor

--- src/test/java/perullo/petprojects/measurements/MeasurementTest.java
+++ src/test/java/perullo/petprojects/measurements/MeasurementTest.java
@@ -12,15 +12,17 @@ public class MeasurementTest {
 
 	@Test
 	public void testDistanceUnits() {
-		Measurement<DistanceUnit> measurement;
-		double actual;
-		double expected;
 
-		measurement = Measurement.of(1.0, DistanceUnit.METERS);
-		expected = 3.280839895013123;
-		actual = measurement.doubleValueIn(DistanceUnit.FEET);
-		softly.assertThat(actual).isCloseTo(expected, tolerance);
+		assertUnitConversion(1.0, DistanceUnit.METERS, DistanceUnit.FEET, 3.280839895013123);
+		assertUnitConversion(1.0, DistanceUnit.FEET, DistanceUnit.METERS, 0.3048);
 
 		softly.assertAll();
+	}
+
+	private <U extends Unit> void assertUnitConversion(double value, U unitToConvertFrom, U unitToConvertTo,
+			double expected) {
+		Measurement<U> measurement = Measurement.of(value, unitToConvertFrom);
+		double actual = measurement.doubleValueIn(unitToConvertTo);
+		softly.assertThat(actual).isCloseTo(expected, tolerance);
 	}
 }
